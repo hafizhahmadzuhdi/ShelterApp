ff715137d436fba1374d9f44eb92ec9d28b46ade
Do, Nhi E <nhiestherdo@gmail.com>
2018-10-01 14:55:26 +0200

    Owner Class

---
+++ ShelterApplication/Owner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShelterApplication
+{
+    class OwnerClass
+    {
+        public OwnerClass()
+        {
+
+        }
+
+        private int ownerID, DoB
+    }
+}
